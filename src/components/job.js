47bdeadeae67ba24379aad374bd00fbf527606a3
Fernando Andrés Avalos López <fa.avalos@uniandes.edu.co>
2021-11-29 12:29:37 -0500

    Added changes and changed locales

--- src/components/job.js
+++ src/components/job.js
@@ -1,14 +1,34 @@
 import React from "react";
+import { FormattedDate, FormattedMessage } from "react-intl";
 
 const Job = (props) => {
+
+  let units;
+  let salary = parseInt(props.offer.salary);
+
+  if (salary === 1) {
+    units = <FormattedMessage id="MillionSingular"/>
+  } else {
+    units = <FormattedMessage id="MillionPlural"/>
+  }
+
   return (
     <tr>
       <th scope="row">{props.offer.id}</th>
       <td>{props.offer.name}</td>
       <td>{props.offer.company}</td>
-      <td>{props.offer.salary}</td>
+      <td>{props.offer.salary} {units}</td>
       <td>{props.offer.city}</td>
-      <td>{props.offer.date}</td>
+      <td>
+        <FormattedDate
+          value={new Date(props.offer.date)}
+          year='numeric'
+          month='long'
+          day='numeric'
+          weekday='long'
+        />
+      </td>
+      <td>{(props.offer.views).toLocaleString()}</td>
     </tr>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { IntlProvider } from "react-intl";
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
 
 import JobsList from "./components/jobslist";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+function getMessages(locale) {
+    if (language === 'es')
+        return localeEsMessages;
+    else
+        return localeEnMessages;
+} 
+
+let language = (navigator.language).split('-')[0];
+
+ReactDOM.render(
+    <IntlProvider locale={language} messages={getMessages(language)}>
+        <JobsList />
+    </IntlProvider>, document.getElementById("root")
+);
